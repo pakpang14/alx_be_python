c73407d592a98f6f3ff1cc56e5c064f2b91c3d1b
pakpang14 <ibrahim.ammed90@gmail.com>
2024-09-19 18:25:19 +0000

    future age calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+current_age = int(input("How old are you?"))
+current_year = 2023
+future_age = ((2050 - current_year) + current_age)
+if future_age == 57:
+    print("In 2050, you will be 57 years old.")
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,4 +1,4 @@
 hours = 2
-seconds = hours *3600
+seconds = hours * 3600
 if seconds == 7200:
     print ("2 hour(s) is 7200 seconds")
