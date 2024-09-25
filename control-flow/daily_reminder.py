2ed5c9e3fd07f55d0ff66f8b98deed9f6549c98a
pakpang14 <ibrahim.ammed90@gmail.com>
2024-09-25 23:16:01 +0000

    Amendment to variable identification in line3

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,15 +1,15 @@
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
-time = input("Is it time-bound? (yes/no): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 match priority :
     case 'high':
-        if priority == 'high' and time == 'yes':
+        if priority == 'high' and time_bound == 'yes':
             print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
     case 'medium' :
-        if priority == "medium" and time == "yes" :
-            print (f"{task}, is a medium priority task, make sure you've handled all high priority tasks")
+        if priority == "medium" and time_bound == "yes" :
+            print (f"{task}, is a medium priority task, make sure you've handled all high priority tasks.")
     case "low":
-        if priority == "low" and time == "no" :
+        if priority == "low" and time_bound == "no" :
             print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
     case _:
-            print("No found task matching this")
+            print("No found task matching your input.")
