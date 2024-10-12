adbaae4a567d17de72b655787cb9ba36bb424442
pakpang14 <ibrahim.ammed90@gmail.com>
2024-10-12 15:53:39 +0000

    More amedment to main and arithmetic operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,15 @@
 
 def perform_operation(num1,num2,operation):
-    if operation == 'add':
-        return num1 + num2
-    elif operation == 'subtract':
-        return num1 - num2
-    elif operation == 'multiply':
-        return num1 * num2
-    elif operation == 'divide':
-        if num2 == 0 :
+    if operation=='add':
+        return num1+num2
+    elif operation=='subtract':
+        return num1-num2
+    elif operation=='multiply':
+        return num1*num2
+    elif operation=='divide':
+        if num2==0:
             return 'Caution! cannot divide by zero.'
         else:
-            return num1 / num2
+            return num1/num2
     else:
         return 'Invalid operation'
