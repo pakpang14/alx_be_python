c19c08cb1bcbd803ffcbc460ffe94c7b894c2f39
pakpang14 <ibrahim.ammed90@gmail.com>
2024-10-13 14:27:04 +0000

    Amendment to add item

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,11 +13,11 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter items: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             pass
         elif choice == '2':
-            item = input("Enter item to remove:" " ")
+            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"{item} has been removed!")
