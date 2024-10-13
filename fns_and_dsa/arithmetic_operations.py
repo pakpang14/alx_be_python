93551bcd57d82983ad9ee46f4afb1bc12b89a2dc
pakpang14 <ibrahim.ammed90@gmail.com>
2024-10-13 03:31:09 +0000

    Division by zero recoded

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
         return num1*num2
     elif operation=="divide":
         if num2==0:
-            return "Cannot divide by zero!"
+            return "Division by zero not allowed!"
         else:
             return num1/num2
     else:
