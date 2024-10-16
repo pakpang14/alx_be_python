2980b37584558b94266413adebb087bd1a15337c
pakpang14 <ibrahim.ammed90@gmail.com>
2024-10-16 03:28:26 +0000

    Bank account Activity

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,13 +1,8 @@
-# number1 = 10
-# number2 = 5
-# z = number1 + number2
-# print (z)
-# z = number1 - number2
-# print (z)
-# z = number1 * number2
-# print (z)
-
-list=["sandals", "bags", "shoes", "bags", "watches", "snikkers"]
-# print(list)
-list.remove("snikkers")
-print(list)
+number1 = 10
+number2 = 5
+z = number1 + number2
+print (z)
+z = number1 - number2
+print (z)
+z = number1 * number2
+print (z)
