12375d9566a663ef1411d4221de56777a9c02586
pakpang14 <ibrahim.ammed90@gmail.com>
2024-10-17 12:53:14 +0000

    codes in line17 amended

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -14,15 +14,15 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(6, -4), 2)
 
 
-    def test_subtract(self):
-        """Test the addition method."""
+    def test_subtraction(self):
+        """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(3, 2), 1)
         self.assertEqual(self.calc.subtract(-2, 1), -3)
         self.assertEqual(self.calc.subtract(4, -1), 5)
 
 
     def test_multiply(self):
-        """Test the addition method."""
+        """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-2, 1), -2)
         self.assertEqual(self.calc.multiply(3, -2), -6)
@@ -30,8 +30,8 @@ class TestSimpleCalculator(unittest.TestCase):
 
 
     def test_divide(self):
-        """Test the addition method."""
-        self.assertEqual(self.calc.divide(10, 0), 2)
+        """Test the division method."""
+        self.assertEqual(self.calc.divide(10, 5), 2)
         self.assertEqual(self.calc.divide(6, -1), -6)
         self.assertEqual(self.calc.divide(-4, 2), -2)
 
