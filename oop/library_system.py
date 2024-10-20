00d7ec6d25fa4ceec19d4bed0fc604eed166999a
pakpang14 <ibrahim.ammed90@gmail.com>
2024-10-20 16:31:32 +0000

    KG for file size add to Ebook output

--- oop/library_system.py
+++ oop/library_system.py
@@ -22,7 +22,7 @@ class Library:
             pass
         for book in self.books:
             if isinstance(book,EBook):
-                print(f"EBook: {book.title} by {book.author}, File size: {book.file_size}")
+                print(f"EBook: {book.title} by {book.author}, File size: {book.file_size}KB")
             elif isinstance(book, PrintBook):
                 print(f"Printbook: {book.title} by {book.author}, Page Count: {book.page_count}")
             else:
