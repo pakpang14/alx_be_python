2667e040cf131bb62226b0b4c72ff96fd68e9447
pakpang14 <ibrahim.ammed90@gmail.com>
2024-10-20 14:55:33 +0000

    Library system exploring inheritance and composition

---
+++ oop/library_system.py
@@ -0,0 +1,20 @@
+class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+class EBook(Book):
+    def __init__(self,file_size):
+        self.file_size = file_size
+class PrintBook(Book):
+    def __init__(self,page_count):
+        self.page_count = page_count
+        
+class Library:
+    def __init__(self):
+        self.books = []
+    def add_book(self,book):
+        self.books.append(book)
+    def list_books(self):
+        for book in self.books:
+            return book
+    
