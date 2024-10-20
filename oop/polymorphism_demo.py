ba78f417e272210103e38df94d81e38890f66a0e
pakpang14 <ibrahim.ammed90@gmail.com>
2024-10-20 19:12:25 +0000

    final amendment to polymorphism-demo

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,21 +1,16 @@
 import math
 class Shape:
     def area(self):
-        pass
+        raise NotImplementedError
 class Rectangle(Shape):
     def __init__(self,length,width):
-        super().__init__(self)
         self.length = length
         self.width = width
-        area = length * width
+    def area(self):
+        return self.length *self.width
 class Circle(Shape):
     def __init__(self,radius):
-        super().__init__(self)
         self.radius = radius
-        area = (math.pi)*radius**2
-
-        for area in Shape:
-            if isinstance(self.length,self.width):
-                print("The area of the Rectangle is: {area}")
-            else:
-                print("The area of the Circle is: {area}")
+    def area(self):
+        return math.pi * self.radius**2
+        
