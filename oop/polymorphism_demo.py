874635833345413362b78094322c1ae5664d75c6
pakpang14 <ibrahim.ammed90@gmail.com>
2024-10-20 18:15:58 +0000

    simple area calculator exploring polymorphism

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,21 @@
+import math
+class Shape:
+    def area(self):
+        pass
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        super().__init__(self)
+        self.length = length
+        self.width = width
+        area = length * width
+class Circle(Shape):
+    def __init__(self,radius):
+        super().__init__(self)
+        self.radius = radius
+        area = (math.pi)*radius*radius
+
+        for area in Shape:
+            if isinstance(self.length,self.width):
+                print("The area of the Rectangle is: {area}")
+            else:
+                print("The area of the Circle is: {area}")
